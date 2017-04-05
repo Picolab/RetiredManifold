2003210fa81c9d80815b99666e07b6f43a612a05
Adam Burdett <burdettadam@gmail.com>
2017-04-05 14:19:57 -0600

    clientKey for live website

--- app/scripts/core/directives/manifoldAuth.js
+++ app/scripts/core/directives/manifoldAuth.js
@@ -4,9 +4,9 @@
     window.manifoldAuth = {};
 
     // ------------------------------------------------------------------------
-    manifoldAuth.clientKey = "7001E532-04FC-11E7-B4DC-51DDE71C24E1";
+    manifoldAuth.clientKey = "701FA2C0-04FC-11E7-B4DC-51DDE71C24E1";
     manifoldAuth.anonECI = "85255500-0b65-0130-243c-00163ebcdddd"; // used for login not authorization?
-    manifoldAuth.callbackURL = "http://localhost:9000/#/code";
+    manifoldAuth.callbackURL = "https://picolab.github.io/manifold/#/code";
     manifoldAuth.host = "kibdev.kobj.net"; // change to cs.kobj.net when in production
     manifoldAuth.login_server = "kibdev.kobj.net"; // change to accounts.kobj.net when in production
     manifoldAuth.defaultECI = "none";
