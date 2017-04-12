bc2523da02892ce8c100f814751236dac93ae97e
Adam Burdett <burdettadam@gmail.com>
2017-04-12 13:23:42 -0600

    config file

--- app/scripts/core/directives/manifoldAuth.js
+++ app/scripts/core/directives/manifoldAuth.js
@@ -3,14 +3,7 @@
 {
     window.manifoldAuth = {};
 
-    // ------------------------------------------------------------------------
-    manifoldAuth.clientKey = "701FA2C0-04FC-11E7-B4DC-51DDE71C24E1";
-    manifoldAuth.anonECI = "85255500-0b65-0130-243c-00163ebcdddd"; // used for login not authorization?
-    manifoldAuth.callbackURL = "https://picolab.github.io/manifold/#/code";
-    manifoldAuth.host = "kibdev.kobj.net"; // change to cs.kobj.net when in production
-    manifoldAuth.login_server = "kibdev.kobj.net"; // change to accounts.kobj.net when in production
-    manifoldAuth.defaultECI = "none";
-    manifoldAuth.access_token = "none";
+  
 
     // ========================================================================
     // OAuth functions
