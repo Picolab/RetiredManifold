186a804aeeb2d9bd83aa6373a650e0fb50ec942e
Adam Burdett <burdettadam@gmail.com>
2017-04-17 13:39:26 -0600

    add local development.

---
+++ app/scripts/production-auth-config.js
@@ -0,0 +1,14 @@
+    // productions ------------------------------------------------------------------------
+    manifoldAuth.clientKey = "701FA2C0-04FC-11E7-B4DC-51DDE71C24E1";
+    manifoldAuth.callbackURL = "https://picolab.github.io/manifold/#/code";
+
+    // localhost ------------------------------------------------------------------------
+    //manifoldAuth.clientKey = "7001E532-04FC-11E7-B4DC-51DDE71C24E1";
+    //manifoldAuth.callbackURL = "http://localhost:9000/#/code";
+
+    manifoldAuth.anonECI = "85255500-0b65-0130-243c-00163ebcdddd"; // used for login not authorization?
+    
+    manifoldAuth.host = "kibdev.kobj.net"; // change to cs.kobj.net when in production
+    manifoldAuth.login_server = "kibdev.kobj.net"; // change to accounts.kobj.net when in production
+    manifoldAuth.defaultECI = "none";
+    manifoldAuth.access_token = "none";
